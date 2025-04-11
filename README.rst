5b050ee828e0c66abe183b1f958d77b70ad724e0
gressho <gressho@uni-muenster.de>
2025-04-11 10:09:24 +0200

    doc: add configuration to README.rst.

--- README.rst
+++ README.rst
@@ -5,17 +5,65 @@
     modify it under the terms of the MIT License; see LICENSE file for more
     details.
 
-======================
+=======================
  invenio-pidstore-extra
-======================
+=======================
 
 Module that adds URN minting by DNB epicur service.
 
-InvenioRDM supports DOI minting with Datacite out of the box. This module adds support for UTRN minting by using the URN API published by German National Library (DNB).
+InvenioRDM supports DOI minting with Datacite out of the box. This module adds support for URN minting by using the URN API published by German National Library (DNB).
+
+Configuration
+=============
+
+.. code-block:: python
+
+    PIDSTORE_EXTRA_DNB_SANDBOX_URI = "http://127.0.0.1:8000/"
+
+    PIDSTORE_EXTRA_DNB_ENABLED = True
+    PIDSTORE_EXTRA_DNB_USERNAME = "username"
+    PIDSTORE_EXTRA_DNB_PASSWORD = "password"
+    PIDSTORE_EXTRA_DNB_ID_PREFIX = "urn:nbn:de:hbz:6"
+    PIDSTORE_EXTRA_FORMAT = "{prefix}-{id}"
+    PIDSTORE_EXTRA_TEST_MODE = True
+
+    #
+    # Persistent identifiers configuration
+    #
+    from invenio_rdm_records.config import RDM_PERSISTENT_IDENTIFIER_PROVIDERS as DEFAULT_PERSISTENT_IDENTIFIER_PROVIDERS
+    from invenio_pidstore_extra import providers
+
+    RDM_PERSISTENT_IDENTIFIER_PROVIDERS = DEFAULT_PERSISTENT_IDENTIFIER_PROVIDERS + [
+        providers.DnbUrnProvider(
+          "urn",
+          client=providers.DNBUrnClient("dnb", PIDSTORE_EXTRA_DNB_ID_PREFIX, PIDSTORE_EXTRA_DNB_USERNAME, PIDSTORE_EXTRA_DNB_PASSWORD, PIDSTORE_EXTRA_FORMAT),
+          label=_("URN"),
+        ),
+    ]
+
+    from invenio_rdm_records.config import RDM_PERSISTENT_IDENTIFIERS as DEFAULT_PERSISTENT_IDENTIFIERS
+
+    RDM_PERSISTENT_IDENTIFIERS = {
+        **DEFAULT_PERSISTENT_IDENTIFIERS,
+        "urn": {
+          "providers": ["urn"],
+          "required": True,
+          "label": _("URN"),
+          "is_enabled": providers.DnbUrnProvider.is_enabled,
+        },
+    }
+
+    from invenio_pidstore_extra.services.components import URNRelationsComponent
+    from invenio_rdm_records.services.components import DefaultRecordsComponents
+
+    RDM_RECORDS_SERVICE_COMPONENTS = DefaultRecordsComponents + [URNRelationsComponent]
+
+Development
+===========
+
 The flask app `invenio_pidstore_extra.dnb-sandbox.py` contains a very simple implementation of the DNB api to allow testing the URN support when the DNB sandbox is not available or just no user has been provided. You can start it with calling
 
-.. raw::
+.. code-block:: console
+
    flask --app invenio_pidstore_extra.dnb-sandbox run --port=8000
-
-
 
--- invenio_pidstore_extra/dnb-sandbox.py
+++ invenio_pidstore_extra/dnb-sandbox.py
@@ -1,6 +1,7 @@
-import arrow
 import base64
 import random
+
+import arrow
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -10,51 +11,60 @@ namespace = "urn:nbn:de:hbz:6-"
 @app.route("/namespaces/name/<name>", methods=["GET"])
 def namespace_info(name: str):
     now = arrow.utcnow().isoformat()
-    return jsonify({
-        "name": name,
-        "created": now,
-        "lastModified": now,
-        "allowsRegistration": True,
-        "owner": "http://localhost:8000/organisations/id/1",
-        "urnNamingPolicy": "http://localhost:8000/policies/urn-naming/id/no-check",
-        "urlPolicy": "http://localhost:8000/policies/url/id/no-check",
-        "resolverUrl": "",
-        "urns": request.url + "/urns",
-        "urnSuggestion": request.url + "/urn-suggestion",
-        "self": request.url
-    })
+    return jsonify(
+        {
+            "name": name,
+            "created": now,
+            "lastModified": now,
+            "allowsRegistration": True,
+            "owner": "http://localhost:8000/organisations/id/1",
+            "urnNamingPolicy": "http://localhost:8000/policies/urn-naming/id/no-check",
+            "urlPolicy": "http://localhost:8000/policies/url/id/no-check",
+            "resolverUrl": "",
+            "urns": request.url + "/urns",
+            "urnSuggestion": request.url + "/urn-suggestion",
+            "self": request.url,
+        }
+    )
 
 
 @app.route("/namespaces/name/<name>/urn-suggestion", methods=["GET"])
 def suggest(name: str):
     now = arrow.utcnow().isoformat()
-    return jsonify({
-        "suggestedUrn": name + str(random.randrange(1000000000, 9999999999, 1)),
-        "namespace": request.url[:request.url.find("/urn-suggestion")],
-        "self": request.url
-    })
+    return jsonify(
+        {
+            "suggestedUrn": name + str(random.randrange(1000000000, 9999999999, 1)),
+            "namespace": request.url[: request.url.find("/urn-suggestion")],
+            "self": request.url,
+        }
+    )
 
 
 @app.route("/urns", methods=["POST"])
 def register():
     now = arrow.utcnow().isoformat()
     content = request.json
-    return jsonify({
-        "urn": content["urn"],
-        "created": now,
-        "lastModified": now,
-        "namespace": "http://localhost:8000/namespaces/name/" + namespace,
-        "successor": None,
-        "urls": request.url + "/urn/" + content["urn"] + "/urls",
-        "myUrls": request.url + "/urn/" + content["urn"] + "/my-urls",
-        "self": request.url + "/urn/" + content["urn"],
-    }), 201
+    return (
+        jsonify(
+            {
+                "urn": content["urn"],
+                "created": now,
+                "lastModified": now,
+                "namespace": "http://localhost:8000/namespaces/name/" + namespace,
+                "successor": None,
+                "urls": request.url + "/urn/" + content["urn"] + "/urls",
+                "myUrls": request.url + "/urn/" + content["urn"] + "/my-urls",
+                "self": request.url + "/urn/" + content["urn"],
+            }
+        ),
+        201,
+    )
 
 
 @app.route("/urns/urn/<urn>", methods=["HEAD"])
 def check_existence(urn: str):
     if urn.startswith(namespace):
-        suffix = urn[len(namespace):]
+        suffix = urn[len(namespace) :]
         if int(suffix) < 5000000000:
             return "", 200
     return "", 404
@@ -63,19 +73,21 @@ def check_existence(urn: str):
 @app.route("/urns/urn/<urn>", methods=["GET"])
 def get_urn(urn: str):
     if urn.startswith(namespace):
-        suffix = urn[len(namespace):]
+        suffix = urn[len(namespace) :]
         if int(suffix) < 5000000000:
             now = arrow.utcnow().isoformat()
-            return jsonify({
-                "urn": urn,
-                "created": now,
-                "lastModified": now,
-                "namespace": "http://localhost:8000/namespaces/name/" + namespace,
-                "successor": None,
-                "urls": request.url + "/urls",
-                "myUrls": request.url + "/my-urls",
-                "self": request.url,
-            })
+            return jsonify(
+                {
+                    "urn": urn,
+                    "created": now,
+                    "lastModified": now,
+                    "namespace": "http://localhost:8000/namespaces/name/" + namespace,
+                    "successor": None,
+                    "urls": request.url + "/urls",
+                    "myUrls": request.url + "/my-urls",
+                    "self": request.url,
+                }
+            )
     return "", 404
 
 
@@ -92,30 +104,36 @@ def get_my_url(urn: str):
 @app.route("/urns/urn/<urn>/urls/base64/<encoded>", methods=["GET"])
 def get_single_url(urn: str, encoded: str):
     now = arrow.utcnow().isoformat()
-    return jsonify({
-        "url": base64.b64decode(bytes(encoded, "utf-8")).decode("utf-8"),
-        "created": now,
-        "lastModified": now,
-        "urn": request.url[:request.url.find("/urls")],
-        "owner": "http://localhost:8000/organisations/id/1",
-        "priority": 10,
-        "self": request.url,
-    })
+    return jsonify(
+        {
+            "url": base64.b64decode(bytes(encoded, "utf-8")).decode("utf-8"),
+            "created": now,
+            "lastModified": now,
+            "urn": request.url[: request.url.find("/urls")],
+            "owner": "http://localhost:8000/organisations/id/1",
+            "priority": 10,
+            "self": request.url,
+        }
+    )
 
 
 @app.route("/urns/urn/<urn>/urls", methods=["POST"])
 def add_url(urn: str):
     now = arrow.utcnow().isoformat()
     content = request.json
-    return jsonify({
-        "url": content["url"],
-        "created": now,
-        "lastModified": now,
-        "urn": request.url[:request.url.find("/urls")],
-        "owner": "http://localhost:8000/organisations/id/1",
-        "priority": 10,
-        "self": request.url + "/urls/base64/" + base64.b64encode(bytes(content["url"], "utf-8")).decode("utf-8"),
-    })
+    return jsonify(
+        {
+            "url": content["url"],
+            "created": now,
+            "lastModified": now,
+            "urn": request.url[: request.url.find("/urls")],
+            "owner": "http://localhost:8000/organisations/id/1",
+            "priority": 10,
+            "self": request.url
+            + "/urls/base64/"
+            + base64.b64encode(bytes(content["url"], "utf-8")).decode("utf-8"),
+        }
+    )
 
 
 @app.route("/urns/urn/<urn>/urls/base64/<encoded>", methods=["DELETE"])
@@ -136,26 +154,30 @@ def set_successor(urn: str):
 
 def get_urls(urn: str, query: str):
     if urn.startswith(namespace):
-        suffix = urn[len(namespace):]
+        suffix = urn[len(namespace) :]
         if int(suffix) < 5000000000:
             now = arrow.utcnow().isoformat()
             find_urls = request.url.rfind(query)
-            return jsonify({
-              "totalItems": 1,
-              "items": [
+            return jsonify(
                 {
-                  "url": "http://example.org/document-url",
-                  "created": now,
-                  "lastModified": now,
-                  "urn": request.url[:find_urls],
-                  "owner": "http://localhost:8000/organisations/id/1",
-                  "priority": 10,
-                  "self": request.url[:find_urls] + "/urls/base64/aHR0cDovL2V4YW1wbGUub3JnL2RvY3VtZW50LXVybA=="
+                    "totalItems": 1,
+                    "items": [
+                        {
+                            "url": "http://example.org/document-url",
+                            "created": now,
+                            "lastModified": now,
+                            "urn": request.url[:find_urls],
+                            "owner": "http://localhost:8000/organisations/id/1",
+                            "priority": 10,
+                            "self": request.url[:find_urls]
+                            + "/urls/base64/aHR0cDovL2V4YW1wbGUub3JnL2RvY3VtZW50LXVybA==",
+                        }
+                    ],
+                    "self": request.url,
                 }
-              ],
-              "self": request.url
-            })
+            )
     return "", 404
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
